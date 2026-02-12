fba523047a6fd33a5f4d35b681334d811f1e7624
Athos Danilo <athosdanilomarques@gmail.com>
2026-02-12 11:50:40 -0300

    Ajustes

--- frontend/js/entrar.js
+++ frontend/js/entrar.js
@@ -6,7 +6,7 @@ const iconeOlho = document.querySelector('.icone-ver-senha');
 const formulario = document.querySelector('.login-formulario');
 const bolinhas = document.querySelectorAll('.bola');
 
-// ---> Carrosel das Imagens e Bolinhas.
+// ---> Carrossel das Imagens e Bolinhas.
 let indiceAtual = 0;
 
 function trocarImagemBolinha() {
@@ -23,3 +23,36 @@ function trocarImagemBolinha() {
 
 }
 setInterval(trocarImagemBolinha, 5000);
+
+// ---> Ver Senha.
+iconeOlho.addEventListener('click', function() {
+    if (senha.type === 'password') {
+        senha.type = 'text';
+        senha.placeholder = 'Senha Visível';
+        iconeOlho.src = '../img/icone-olho-aberto.png';
+    }
+    else {
+        senha.type = 'password';
+        senha.placeholder = 'Senha';
+        iconeOlho.src = '../img/icone-olho-fechado.png'
+    }
+});
+
+// ---> Login.
+formulario.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const usuarioDigitado = nomeUsuario.value;
+    const senhaDigitada = senha.value;
+
+    if (usuarioDigitado === "" || senhaDigitada === "") {
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    console.log("Tentativa de Login");
+    console.log("Usuário:", usuarioDigitado);
+    console.log("Senha:", senhaDigitada);
+
+    alert("Login realizado com sucesso! Bem-vindo(a), " + usuarioDigitado + "!");
+});
